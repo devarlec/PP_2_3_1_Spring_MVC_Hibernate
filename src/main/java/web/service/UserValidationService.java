e8f8d5904ced9f975ac08e328d3802d16a70fb0f
evgen <devarlec@gmail.com>
2025-04-14 03:56:31 +0400

    added hashCode() and equals(Object o)

---
+++ src/main/java/web/service/UserValidationService.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import web.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Service("userValidationService")
+public class UserValidationService {
+
+	// "внедряется экземпляр javax.validation.Validator"
+	@Autowired
+	private Validator validator;
+
+	public Set<ConstraintViolation<User>> validateUser(User user) {
+		return validator.validate(user);
+	}
+}
